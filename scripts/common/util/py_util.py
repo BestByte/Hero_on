d15dbd509d1288dddca4526bf0a6e83bd7da02ea
飞 <ilvxna@hotmail.com>
2016-10-04 12:18:50 +0800

    get_number_secs

--- scripts/common/util/py_util.py
+++ scripts/common/util/py_util.py
@@ -1,8 +1,8 @@
 import time
 _ONEDAY_SECONDS = 24*60*60
 #生成一个日期的整数，默认以凌晨4:00为界
-def get_number_date(frome_time,from_clock):
-	now_time=time.time()
+def get_number_date(from_time,from_clock):
+	now_time=from_time or time.time()
 	_from_clock=from_clock or 4
 	now_time_table=time.struct_time(now_time)
 	if now_time_table==None:
@@ -14,4 +14,23 @@ def get_number_date(frome_time,from_clock):
 		return now_time_table[0]*1000+now_time_table[1]*100+now_time_table[2]
 
 #获得某日某个时刻点对应的秒数
+def get_number_secs(from_time,from_clock):
+	now_time=from_time or time.time()
+	_from_clock=from_clock or 4
+	now_time_table=time.struct_time(now_time)
+	if now_time==None:
+		return 0
+	if now_time_table[0]<from_clock:
+		now_time_table_2=time.struct_time(now_time-_ONEDAY_SECONDS)
+		now_time_table_2[3]=from_clock
+		now_time_table_2[4]=0
+		now_time_table_2[5]=0
+		return now_time_table_2
+	else:
+		now_time_table[3]=from_clock
+		now_time_table[4]=0
+		now_time_table[5]=0
+		return now_time_table
+
+
 
