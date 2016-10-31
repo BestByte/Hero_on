84a2fa791214a2b81f7ba1f7608b3380f2915c04
飞 <ilvxna@hotmail.com>
2016-10-31 11:48:34 +0800

    action config

--- scripts/base/SubSystem/ActivitySystem.py
+++ scripts/base/SubSystem/ActivitySystem.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import KBEDebug
 import py_util
 
---
+++ scripts/base/SubSystem/ArenaSystem.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import py_util
+
+pvp_type_weak   = 1 
+pvp_type_strong = 2
+pvp_type_enemy  = 3
+
+score_type_day  = 1
+score_type_week = 2
+
+class ArenaSystem:
+	def __init__(self):
+		arenicDataKey={}
+		
