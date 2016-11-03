711542886399ec8896a182688105a6554f50e9d0
飞 <ilvxna@hotmail.com>
2016-11-03 09:33:35 +0800

    up

---
+++ scripts/common/config/PriceList.py
@@ -0,0 +1,17 @@
+﻿import py_util
+from KBEDebug import *
+class PriceList:
+	#管理物品价格列表
+	def __init__(self):
+		cfgData = py_util._readXml("/data/xml/PriceList.xml", "id_i")
+		self.cfgData = cfgData
+		for _,v in self.cfgData.items():
+			self.VerifyData(v)
+
+	def VerifyData(self,itemData):
+		pass
+	#--获取对应类型物品的价格定义
+	#--id表示表中物品的索引值
+	def GetPriceData(self,idx):
+		if self.cfgData:
+			return self.cfgData[idx]
