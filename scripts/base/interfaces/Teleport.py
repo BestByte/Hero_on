5121094b85f5dff6de61a2a8b3e5c197fccf60ed
飞 <ilvxna@hotmail.com>
2016-12-12 17:36:48 +0800

    up

--- scripts/base/interfaces/Teleport.py
+++ scripts/base/interfaces/Teleport.py
@@ -11,13 +11,7 @@ class Teleport:
 		# 因为副本是需要钥匙开启的，所有的副本都使用实体SpaceDuplicate创建
 		# 因此我们只需要简单判断当前spaceUType所对应的配置中场景的脚本类型是否包含"Duplicate"
 		# 就能得出是否在一个副本中
-		spacedatas = d_spaces.datas[self.cellData["spaceUType"]]
-		avatar_inittab = d_avatar_inittab.datas[self.roleType]
-
-		if "Duplicate" in spacedatas["entityType"]:
-			self.cellData["spaceUType"] = avatar_inittab["spaceUType"]
-			self.cellData["direction"] = (0, 0, avatar_inittab["spawnYaw"])
-			self.cellData["position"] = avatar_inittab["spawnPos"]
+		pass
 
 	#--------------------------------------------------------------------------------------------
 	#                              Callbacks
@@ -31,18 +25,6 @@ class Teleport:
 		# 防止使用同一个号登陆不同的demo造成无法找到匹配的地图从而无法加载资源导致无法进入游戏
 		# 这里检查一下， 发现不对则强制同步到匹配的地图
 		# 忽略机器人的检查
-		if hasattr(self, "cellData") and self.getClientType() != 6:
-			# 如果角色跳转到了同属某个demo的其他场景那么不强制回到出生的主场景
-			if self.cellData["spaceUType"] in GlobalConst.g_demoMaps.values():
-				spaceUType = GlobalConst.g_demoMaps.get(self.getClientDatas(), 1)
-
-				if self.cellData["spaceUType"] != spaceUType:
-					spacedatas = d_spaces.datas[spaceUType]
-					self.spaceUTypeB = spaceUType
-					self.cellData["spaceUType"] = spaceUType
-					self.cellData["position"] = spacedatas.get("spawnPos", (0,0,0))
-		
-		KBEngine.globalData["Spaces"].loginToSpace(self, self.spaceUTypeB, {})
-
+		pass
 
 
